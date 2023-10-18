9b11a9ba9784703d3e9d5471d30d8dabd6ea8acc
SreeDeekshithaEswaran <126445402+SreeDeekshithaEswaran@users.noreply.github.com>
2023-10-18 21:04:47 +0530

    Created build

---
+++ public/components/footer.js
@@ -0,0 +1,51 @@
+const footer = () => {
+return `
+<div style="margin-top: 1rem;
+display: flex;
+list-style: none;">
+    <div style="line-height: 2rem;
+  margin-left: 1rem;">
+        // <li style="color:white" style="color:white"><button  Edu4all Chat Room</li>
+        <ul>
+        <li><a href="https://your-deployed-project-url.com" target="_blank" style="color: black;">Edu4all Chat Room</a></li>
+    </ul>
+        <li style="color:black">Teach Academy</li>
+        <li style="color:black">Get the App</li>
+        <li style="color:black">About Us</li>
+        <li style="color:black">Contact Us</li>
+    </div>
+    <div style="line-height: 2rem;
+  margin-left: 1rem;margin-left: 10rem;">
+        <li style="color:white">Careers</li>
+        <li style="color:white">Blog</li>
+        <li style="color:white">Help and Support</li>
+        <li style="color:white">Affiliate</li>
+        <li style="color:white">Investors</li>
+    </div>
+    <div style="line-height: 2rem;
+  margin-left: 1rem;margin-left: 10rem; color:white !important">
+        <li style="color:white">Terms</li>
+        <li style="color:white">Privacy Policy</li>
+        <li style="color:white">Sitemap</li>
+        <li style="color:white">Accessibility Statement</li>
+    </div>
+    <p style="margin-left: 10rem;" style="margin-top: none;
+    margin-left: 34.5rem;
+    padding-top: 1.5rem;
+    padding: 1rem;
+    border-radius: 10%;
+    font-size: 1.3rem;">English</p>
+</div>
+
+<div style="display: flex;
+height: 100px;">
+    <img style="width: 5%;
+    margin-left: 1rem;
+    margin-top: 1rem;" src="Edu4all_logo.png">
+    <p style=" color:white; position: absolute; bottom:10%; margin-top: 10rem;
+    margin-left: 67rem;">© 2023 Edu4all, Inc.</p>
+</div>
+`;
+};
+
+export default footer;
---
+++ server.js
@@ -0,0 +1,8 @@
+// const express = require('express');
+// const connectDB = require('./db');
+// const app = express();
+// //body parser
+// app.use(express.json());
+
+// //connect to database
+// connectDB();
